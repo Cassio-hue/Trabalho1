6950504d0e0ead215edce50d6e182839697bf920
Cassio-hue <cassiocvtb2003@gmail.com>
2022-06-20 13:43:22 -0300

    estrutura da classe do dominio Nome

--- Nome.cpp
+++ Nome.cpp
@@ -1,11 +1,31 @@
 #include "Nome.h"
 
-Nome::Nome()
-{
-    //ctor
+bool validateNome(string valor){
+    /*
+    Nome e sobrenome
+    Ate 30 caracteres
+    Somente letras (A-Z|a-z) ou espaco em branco
+    Nao pode ter espaco em branco em sequencia
+    Primeiro caracter de prenome ou sobrenome deve ser em maiusculo
+
+    bool = numeroCaracteres;
+    bool = primeiraLetraNome;
+    bool = primeiraLetraSobrenome;
+    bool = sequenciaEspacobranco;
+
+    return (numeroCaracteres && primeiraLetraNome && primeiraLetraSobrenome && sequenciaEspacobranco);
+    */
+   return 0;
 }
 
-Nome::~Nome()
-{
-    //dtor
+bool Nome::setNome(string valor){
+    if (validateNome(valor)){
+        nome = valor;
+        return true;
+    }
+    return false;
+}
+
+string Nome::getNome(){
+    return nome;
 }
--- Nome.h
+++ Nome.h
@@ -1,16 +1,12 @@
 #ifndef NOME_H
 #define NOME_H
 
-
-class Nome
-{
-    public:
-        Nome();
-        virtual ~Nome();
-
-    protected:
-
+class Nome {
     private:
+        string nome;
+    public:
+        bool setNome(string valor);
+        string getNome();
 };
 
 #endif // NOME_H

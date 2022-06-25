497b27b829d6d5511fcd5eb5f3eebee302e81b97
Cassio-hue <cassiocvtb2003@gmail.com>
2022-06-25 08:50:58 -0300

    exclusão das variaveis constante do dominio data

--- Dominios.h
+++ Dominios.h
@@ -47,8 +47,6 @@ class Idioma : public Dominio {
 class Data : public Dominio {
     private:
         void validar(string);
-        static const int TAMANHO_DATA = 6;
-        static const int MESES_ANO = 12;
 };
 
 

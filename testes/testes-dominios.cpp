0b1c1bdddbc3fa938896a8114230fb92e89f45d3
artistrea <apiravesi@gmail.com>
2022-07-04 16:47:55 -0300

    ✨ Testando construtor

--- testes/testes-dominios.cpp
+++ testes/testes-dominios.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 const string NOMES_VALIDOS[] = {
-    "Artur Padovezi", 
+    "Artur Padovezi",
     "A B",
     "Enzo Zanetti",
     "Artur Zbcdefghijklmnopqrstuvw",
@@ -37,7 +37,7 @@ const string NOMES_INVALIDOS[] = {
 
 
 const string NOTAS_VALIDAS[] = {
-    "0", 
+    "0",
     "9",
     "10",
 };
@@ -50,64 +50,118 @@ const string NOTAS_INVALIDAS[] = {
 
 
 
-TEST_CASE("Testando Domínio Nome", "[Nome]"){
-    Nome nome;
-
-    SECTION("Valores validos"){
-
-        for(const string &valor : NOMES_VALIDOS){
-            INFO("O valor é " << valor);
-            CHECK_NOTHROW(nome.setValor(valor));
+TEST_CASE("Testando Domínio Nome", "[Nome]"){ 
+    Nome nome{NOMES_VALIDOS[0]};
+    SECTION("Método set"){
+        SECTION("Valores validos"){
             
-            CHECK(nome.getValor() == valor);
+            for(const string &valor : NOMES_VALIDOS){
+                UNSCOPED_INFO("O valor é " << valor);
+                CHECK_NOTHROW(nome.setValor(valor));
+
+                CHECK((nome.getValor() == valor));
+            }
+
         }
-        
+
+        SECTION("Valores invalidos"){
+            for(const string &valor : NOMES_INVALIDOS){
+                UNSCOPED_INFO("O valor é " << valor);
+
+                CHECK_THROWS_AS(nome.setValor(valor), invalid_argument);
+                CHECK((nome.getValor() != valor));
+
+            }
+        }
     }
+    SECTION("Construtor"){
+        Nome* nomeptr;
+        SECTION("Valores validos"){
+            
+            for(const string &valor : NOMES_VALIDOS){
+                UNSCOPED_INFO("O valor é " << valor);
+                nomeptr = 0;
+                nomeptr = new Nome(valor);
 
-    SECTION("Valores invalidos"){
-        string valor_inicial = "Artur Padovezi";
-        
-        REQUIRE_NOTHROW(nome.setValor(valor_inicial));
-        
-        REQUIRE(nome.getValor() == valor_inicial);
+                CHECK((nomeptr != 0));
+                if(nomeptr != 0)
+                    CHECK((nomeptr->getValor() == valor));
+                
+                delete nomeptr;
+            }
+        }
 
-        for(const string &valor : NOMES_INVALIDOS){
-            UNSCOPED_INFO("O valor é " << valor);
+        SECTION("Valores invalidos"){
 
-            CHECK_THROWS_AS(nome.setValor(valor), invalid_argument);
-            CHECK(nome.getValor() != valor);
+            for(const string &valor : NOMES_INVALIDOS){
+                UNSCOPED_INFO("O valor é " << valor);
+                nomeptr = 0;
+                
+                CHECK_THROWS_AS((nomeptr = new Nome(valor)), invalid_argument);
+                
+                delete nomeptr;
+            }
         }
     }
 }
 
 
 TEST_CASE("Testando Domínio Nota", "[Nota]"){
-    Nota nota;
+    Nota nota{NOTAS_VALIDAS[0]};
 
-    SECTION("Valores validos"){
+    SECTION("Método set"){
+        SECTION("Valores validos"){
 
-        for(const string &valor : NOTAS_VALIDAS){
-            INFO("O valor é " << valor);
-            CHECK_NOTHROW(nota.setValor(valor));
-            
-            CHECK(nota.getValor() == valor);
+            for(const string &valor : NOTAS_VALIDAS){
+                UNSCOPED_INFO("O valor é " << valor);
+                CHECK_NOTHROW(nota.setValor(valor));
+
+                CHECK((nota.getValor() == valor));
+            }
+
         }
-        
+
+        SECTION("Valores invalidos"){
+            for(const string &valor : NOTAS_INVALIDAS){
+                UNSCOPED_INFO("O valor é " << valor);
+
+                CHECK_THROWS_AS(nota.setValor(valor), invalid_argument);
+                CHECK((nota.getValor() != valor));
+            }
+        }
     }
 
-    SECTION("Valores invalidos"){
-        string valor_inicial = "5";
-        
-        REQUIRE_NOTHROW(nota.setValor(valor_inicial));
-        
-        REQUIRE(nota.getValor() == valor_inicial);
+    SECTION("Construtor"){
+        Nota* notaptr;
 
-        for(const string &valor : NOTAS_INVALIDAS){
-            UNSCOPED_INFO("O valor é " << valor);
+        SECTION("Valores validos"){
+            for(const string &valor : NOTAS_VALIDAS){
+                UNSCOPED_INFO("O valor é " << valor);
+                notaptr = 0;
+                
+                CHECK_NOTHROW(notaptr = new Nota(valor));
+                
+                CHECK((notaptr->getValor() == valor));
+                
+                delete notaptr;
+            }
+        }
 
-            CHECK_THROWS_AS(nota.setValor(valor), invalid_argument);
-            CHECK(nota.getValor() != valor);
+        SECTION("Valores invalidos"){
+            for(const string &valor : NOTAS_INVALIDAS){
+                UNSCOPED_INFO("O valor é " << valor);
+                notaptr = 0;
+                
+                CHECK_THROWS_AS(notaptr = new Nota(valor), invalid_argument);
+                
+                delete notaptr;
+            }
         }
     }
 }
+
+
+
+
+
 

873f9cae756500fbb1109de1b26e32e93052917e
Cassio-hue <cassiocvtb2003@gmail.com>
2022-08-19 17:01:47 -0300

    ✨ Introdução de alguns métodos da hospedagem

--- CntlApresentacao.h
+++ CntlApresentacao.h
@@ -1,7 +1,7 @@
 #ifndef CONTROLADORASAPRESENTACAO_H_INCLUDED
 #define CONTROLADORASAPRESENTACAO_H_INCLUDED
 
-//#include <conio.h>
+#include <conio.h>
 #include <iostream>
 #include <string.h>
 
@@ -9,27 +9,35 @@
 #include "Entidades.h"
 #include "Interface.h"
 
+#define CLR_SCR system("cls");
 
 class CntrApresentacaoControle {
     private:
         Email email;
         IApresentacaoAutenticacao *cntrApresentacaoAutenticacao;
         IApresentacaoUsuario *cntrApresentacaoUsuario;
+        IApresentacaoHospedagem *cntrApresentacaoHospedagem;
     public:
         void executar();
         void setCntrApresentacaoAutenticacao(IApresentacaoAutenticacao*);
         void setCntrApresentacaoUsuario(IApresentacaoUsuario*);
+        void setCntrApresentacaoHospedagem(IApresentacaoHospedagem*);
 };
 
 inline void CntrApresentacaoControle::setCntrApresentacaoAutenticacao(IApresentacaoAutenticacao *cntr) {
     cntrApresentacaoAutenticacao = cntr;
 }
 
-inline void CntrApresentacaoControle::setCntrApresentacaoUsuario(IApresentacaoUsuario *cntr){
-            cntrApresentacaoUsuario = cntr;
+inline void CntrApresentacaoControle::setCntrApresentacaoUsuario(IApresentacaoUsuario *cntr) {
+    cntrApresentacaoUsuario = cntr;
+}
+
+inline void CntrApresentacaoControle::setCntrApresentacaoHospedagem(IApresentacaoHospedagem *cntr) {
+    cntrApresentacaoHospedagem = cntr;
 }
 
 
+// ================================================================================
 class CntrApresentacaoAutenticacao:public IApresentacaoAutenticacao {
     private:
         IServicoAutenticacao *cntr;
@@ -43,20 +51,30 @@ inline void CntrApresentacaoAutenticacao::setCntrServicoAutenticacao(IServicoAut
 }
 
 
+// ================================================================================
 class CntrApresentacaoUsuario:public IApresentacaoUsuario {
     private:
         IServicoUsuario *cntrServicoUsuario;
+        IServicoHospedagem *cntrServicoHospedagem;
         void consultarDadosPessoais();
     public:
-        void executar(Email);
         void cadastrar();
+        void executar(Email);
         void setCntrServicoUsuario(IServicoUsuario*);
+        void setCntrServicoHospedagem(IServicoHospedagem*);
 };
 
 inline void CntrApresentacaoUsuario::setCntrServicoUsuario(IServicoUsuario *cntr) {
     cntrServicoUsuario = cntr;
 }
 
+inline void CntrApresentacaoUsuario::setCntrServicoHospedagem(IServicoHospedagem *cntr) {
+    cntrServicoHospedagem = cntr;
+}
+
+
+
+// ================================================================================
 class CntrApresentacaoHospedagem:public IApresentacaoHospedagem {
     private:
         IServicoHospedagem *cntr;
